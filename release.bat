0270cfe935450c293b31adbe2e7c228669f6456e
tim0-12432 <79634593+tim0-12432@users.noreply.github.com>
2026-08-11 23:13:09 +0200

    switch to pnpm and automatic release

--- release.bat
+++ release.bat
@@ -1,12 +1,12 @@
 @echo on
 
-npm login
+@REM npm login
 
 set /p commit=Enter the commit message:
 set /p version=Enter the new version number:
 
-npm run build
-npm publish --access public
+@REM npm run build
+@REM npm publish --access public
 
 git add .
 git commit -S -m "%commit%"
